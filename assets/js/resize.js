57530c1791227feaf70cdc997d24da81a1f8be41
Ramonprog <programador.ramon@gmail.com>
2025-04-01 17:54:28 -0300

    feat: adicionando fake collapse

--- assets/js/resize.js
+++ assets/js/resize.js
@@ -2,15 +2,24 @@ function toggleNav() {
     let ul = document.getElementById("nav-ul");
     let hamburger = document.getElementById("menu-hamburger");
     let userArea = document.getElementById("user-area");
+    let secondCard = document.getElementById("cart-second-section");
+    let footerInfos = document.getElementById("footer-infos");
+    let fakeCollapse = document.getElementById("fake-collapse");
 
     if (window.innerWidth < 768) {
         ul.style.display = "none";
         hamburger.style.display = "block";
         userArea.style.display = "none";
+        secondCard.style.display = "none";
+        footerInfos.style.display = "none";
+        fakeCollapse.style.display = "block";
     } else {
         ul.style.display = "flex";
         hamburger.style.display = "none";
         userArea.style.display = "flex";
+        secondCard.style.display = "flex";
+        footerInfos.style.display = "flex";
+        fakeCollapse.style.display = "none";
     }
 }
 
